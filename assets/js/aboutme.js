ebca34ac72df7c21eeb11de0c601f162f3689a86
Zack <168221270+Zval321@users.noreply.github.com>
2024-08-23 16:32:11 -0500

    new

---
+++ assets/js/aboutme.js
@@ -0,0 +1,9 @@
+// Function to apply fade-in effect
+function fadeIn() {
+    document.body.style.opacity = 1; // Trigger the fade-in effect
+}
+
+// Execute fadeIn function when the page has finished loading
+window.onload = function() {
+    fadeIn();
+};
